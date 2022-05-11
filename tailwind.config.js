065fbd4d74cf12457523cdf675d425b51bd1ffb9
twonarly <72956780+Twonarly1@users.noreply.github.com>
2022-05-10 22:08:13 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        orange: '#F97316',
+        slate: '#E2E8F0',
+      },
+      backgroundImage: {
+        'header-background': "url('/images/header-background.jpg')",
+      },
+    },
+  },
+  plugins: [],
+}
