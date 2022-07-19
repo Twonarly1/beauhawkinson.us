e3f1b76003829a85195a494b23c5f124f610262b
Beau Hawkinson <hawkinson.beau@yahoo.com>
2022-07-19 16:30:14 -0500

    change url

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,10 @@ export default function Footer() {
   return (
     <div className="mt-32 p-4 text-center text-gray-600 dark:text-gray-100">
       © {new Date().getFullYear()} bhawkinson ·{' '}
-      <a className="text-blue-500" href="https://github.com/Twonarly1">
+      <a
+        className="text-blue-500"
+        href="https://github.com/Twonarly1/bhawkinson-twonarly1"
+      >
         Source
       </a>
     </div>
