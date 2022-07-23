ceac43245e807b51b0afe9b47ad94c9a5290b29d
Beau Hawkinson <hawkinson.beau@yahoo.com>
2022-07-23 14:32:12 -0500

    z index buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,7 +10,7 @@ type Props = {
 export default function Button({ children, className, onClick, Icon }: Props) {
   return (
     <div
-      className={`cursor-pointer rounded-full border border-gray-300 bg-gray-50 p-1 hover:border-gray-400 hover:shadow-md dark:border-gray-400 dark:bg-slate-800 dark:hover:border-gray-300 dark:hover:shadow-sm dark:hover:shadow-white ${className}`}
+      className={`z-10 cursor-pointer rounded-full border border-gray-300 bg-gray-50 p-1 hover:border-gray-400 hover:shadow-md dark:border-gray-400 dark:bg-slate-800 dark:hover:border-gray-300 dark:hover:shadow-sm dark:hover:shadow-white ${className}`}
       onClick={onClick}
     >
       <Icon className="h-4 w-4" />
