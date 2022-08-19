0715e7b2efc62afc2334c75456ffb30280d1920e
Beau Hawkinson <hawkinson.beau@yahoo.com>
2022-08-19 07:22:39 -0500

    add coinflip

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,72 +1,69 @@
-import Image from 'next/image'
-import React from 'react'
-import hoverImage from '../../../public/images/profileImages/pfp.jpeg'
-import pfp from '../../../public/images/profileImages/profilepic.jpg'
-import Heading from '../Heading'
+import Image from "next/image"
+import React from "react"
+import hoverImage from "../../../public/images/profileImages/pfp.jpeg"
+import pfp from "../../../public/images/profileImages/profilepic.jpg"
+import Heading from "../Heading"
 
 export default function About() {
-  const size = 128
-  return (
-    <div className="mx-auto  bg-black/5 px-6 pb-32 dark:bg-white/10">
-      <div className="mx-auto w-[128px] pt-16 pb-8 text-center">
-        <div
-          style={{ height: size, width: size }}
-          className="relative flex-col rounded-full"
-        >
-          <Image
-            priority
-            className="rounded-full"
-            src={pfp}
-            alt="Portrait photo of me"
-            objectFit="cover"
-            layout="fill"
-            quality={100}
-          />
-          <div
-            style={{ height: size, width: size }}
-            className="absolute inset-0 flex-col rounded-full bg-white opacity-0 hover:opacity-100"
-          >
-            <Image
-              loading="lazy"
-              className="rounded-full"
-              src={hoverImage}
-              alt="Portrait photo of me"
-              quality={100}
-              objectFit="cover"
-              layout="fill"
-            />
-          </div>
+    return (
+        <div className="mx-auto  bg-black/5 px-6 pb-32 dark:bg-white/10">
+            <div className="flex items-center justify-center gap-x-16 py-6 ">
+                <div className="group h-[127px] w-[127px] cursor-pointer bg-transparent perspective">
+                    <div className="relative h-full w-full duration-1000 preserve-3d group-hover:my-rotate-y-180">
+                        <div className="absolute h-[127px] w-[127px] rounded-full backface-hidden">
+                            <Image
+                                priority
+                                className="rounded-full"
+                                src={pfp}
+                                alt="Portrait photo of me"
+                                objectFit="cover"
+                                layout="fill"
+                                quality={100}
+                            />
+                        </div>
+                        <div className="absolute h-[127px] w-[127px] my-rotate-y-180 backface-hidden ">
+                            <Image
+                                loading="lazy"
+                                className="rounded-full"
+                                src={hoverImage}
+                                alt="Portrait photo of me"
+                                quality={100}
+                                objectFit="cover"
+                                layout="fill"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className=" text-center">
+                <Heading
+                    title={"Hey, I'm Beau!"}
+                    subtitle={"I contribute and develop in the Web3 space."}
+                />
+                <div className="space-x-3 pt-6">
+                    <a
+                        className="rounded bg-blue-200 p-1 px-2 text-blue-800 hover:bg-blue-300"
+                        data-splitbee-event="Open Twitter"
+                        href="https://twitter.com/twonarly"
+                    >
+                        Twitter
+                    </a>
+                    <a
+                        className="rounded bg-gray-300 p-1 px-2 text-gray-800 hover:bg-gray-400"
+                        data-splitbee-event="Open GitHub"
+                        href="https://github.com/Twonarly1"
+                    >
+                        GitHub
+                    </a>
+                    <a
+                        className="rounded bg-purple-200 p-1 px-2 text-purple-800 hover:bg-purple-300"
+                        data-splitbee-event="Open LinkedIn"
+                        href={"/7.17-resume.pdf"}
+                    >
+                        Resume
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="text-center">
-        <Heading
-          title={"Hey, I'm Beau!"}
-          subtitle={'I contribute and develop in the Web3 space.'}
-        />
-        <div className="space-x-3 pt-6">
-          <a
-            className="rounded bg-blue-200 p-1 px-2 text-blue-800 hover:bg-blue-300"
-            data-splitbee-event="Open Twitter"
-            href="https://twitter.com/twonarly"
-          >
-            Twitter
-          </a>
-          <a
-            className="rounded bg-gray-300 p-1 px-2 text-gray-800 hover:bg-gray-400"
-            data-splitbee-event="Open GitHub"
-            href="https://github.com/Twonarly1"
-          >
-            GitHub
-          </a>
-          <a
-            className="rounded bg-purple-200 p-1 px-2 text-purple-800 hover:bg-purple-300"
-            data-splitbee-event="Open LinkedIn"
-            href={'/7.17-resume.pdf'}
-          >
-            Resume
-          </a>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,56 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-  ],
-  darkMode: 'class',
-  theme: {
-    extend: {
-      screens: {
-        xs: '400px',
-      },
-      fontFamily: {
-        lora: "'Lora', serrif",
-      },
-      animation: {
-        blob: 'blob 7s infinite',
-      },
-      keyframes: {
-        blob: {
-          '0%': {
-            transform: 'translate(0px, 0px) scale(1)',
-          },
-          '33%': {
-            transform: 'translate(30px, -50px) scale(1.1)',
-          },
-          '66%': {
-            transform: 'translate(-20px, 20px) scale(0.9)',
-          },
-          '100%': {
-            transform: 'translate(0px, 0px) scale(1)',
-          },
+const plugin = require("tailwindcss/plugin")
+
+const Myclass = plugin(function ({ addUtilities }) {
+    addUtilities({
+        ".my-rotate-y-180": {
+            transform: "rotateY(180deg)",
         },
-      },
-      colors: {
-        'primary-dark': '#272727',
-      },
+        ".preserve-3d": {
+            transformStyle: "preserve-3d",
+        },
+        ".perspective": {
+            perspective: "1000px",
+        },
+        ".backface-hidden": {
+            backfaceVisibility: "hidden",
+        },
+    })
+})
+module.exports = {
+    content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
+    darkMode: "class",
+    theme: {
+        extend: {
+            screens: {
+                xs: "400px",
+            },
+            fontFamily: {
+                lora: "'Lora', serrif",
+            },
+            animation: {
+                blob: "blob 7s infinite",
+            },
+            keyframes: {
+                blob: {
+                    "0%": {
+                        transform: "translate(0px, 0px) scale(1)",
+                    },
+                    "33%": {
+                        transform: "translate(30px, -50px) scale(1.1)",
+                    },
+                    "66%": {
+                        transform: "translate(-20px, 20px) scale(0.9)",
+                    },
+                    "100%": {
+                        transform: "translate(0px, 0px) scale(1)",
+                    },
+                },
+            },
+            colors: {
+                "primary-dark": "#272727",
+            },
+        },
     },
-  },
-  plugins: [require('@tailwindcss/typography')],
+    plugins: [require("@tailwindcss/typography"), Myclass],
 }
