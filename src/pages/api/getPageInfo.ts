abb53f19db9a5eeccd3c689711ff87809f403b1c
Beau Hawkinson <hawkinson.beau@yahoo.com>
2022-09-20 17:26:26 -0500

    sanity-work 1.1

--- src/pages/api/getPageInfo.ts
+++ src/pages/api/getPageInfo.ts
@@ -4,8 +4,8 @@ import { groq } from "next-sanity"
 import { sanityClient } from "../../../sanity"
 import { PageInfo } from "../../../typings"
 
-const query = groq`
-*[_type == "pageInfo"][0]
+const pageInfoQuery = groq`
+  *[_type == "pageInfo"][0]
 `
 
 type Data = {
@@ -13,7 +13,7 @@ type Data = {
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-    const pageInfo: PageInfo = await sanityClient.fetch(query)
+    const pageInfo: PageInfo = await sanityClient.fetch(pageInfoQuery)
 
     res.status(200).json({ pageInfo })
 }
