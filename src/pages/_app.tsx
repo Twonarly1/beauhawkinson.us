14192d197bf7d3208fd4d43572104ce46dd8029a
Beau Hawkinson <hawkinson.beau@yahoo.com>
2022-09-20 19:39:21 -0500

    add Head info

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,20 @@ import "../styles/globals.css"
 import { AppProps } from "../../node_modules/next/app"
 import { ThemeProvider } from "next-themes"
 import { Toaster } from "react-hot-toast"
+import Head from "next/head"
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
+            <Head>
+                <title>bhawkinson</title>
+                <meta name="description" content="View my personal website!" />
+                <meta property="og:title" content="Web Developer. Interested in web3." />
+                <link rel="icon" href="/two.png" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                {/* <script async src="https://cdn.splitbee.io/sb.js"></script> */}
+            </Head>
             <ThemeProvider attribute="class">
                 <Component {...pageProps} />
                 <Toaster position="top-right" reverseOrder={false} />
