421d2723318088904ef67624826a2fa944023fd6
Beau Hawkinson <hawkinson.beau@yahoo.com>
2022-12-13 11:34:53 -0600

    force

--- src/components/sections/about/About.tsx
+++ src/components/sections/about/About.tsx
@@ -8,17 +8,15 @@ type Props = {
     pageInfo: PageInfo
 }
 
-// const NavItems = [
-//     { href: "#projects", title: "Projects" },
-//     { href: "#skills", title: "Technologies" },
-//     { href: "#experiences", title: "Experience" },
-//     { href: "#achievements", title: "Achievements" },
-//     { href: "#contact", title: "Contact" },
-// ]
+const NavItems = [
+    { href: "#projects", title: "Projects" },
+    { href: "#skills", title: "Technologies" },
+    { href: "#experiences", title: "Experience" },
+    { href: "#achievements", title: "Achievements" },
+    { href: "#contact", title: "Contact" },
+]
 
 const About = ({ pageInfo }: Props) => {
-    console.log(pageInfo)
-
     return (
         <div className="h-screen">
             <HeroImage heroImage={pageInfo?.heroImage} />
@@ -26,7 +24,7 @@ const About = ({ pageInfo }: Props) => {
                 <Heading title={pageInfo?.name} subtitle={pageInfo?.role} />
             </div>
 
-            {/* <div className="-mt-4 flex flex-col items-center justify-center gap-2 md:gap-3 2xs:flex-row">
+            <div className="-mt-4 flex flex-col items-center justify-center gap-2 md:gap-3 2xs:flex-row">
                 {NavItems.map((item, i: number) => (
                     <Link key={i} href={item.href}>
                         <button className="z-10 p-1 px-2 text-xs uppercase tracking-widest text-gray-400 transition-all hover:scale-105 hover:text-pink-300 dark:text-gray-400 dark:hover:text-pink-200">
@@ -34,7 +32,7 @@ const About = ({ pageInfo }: Props) => {
                         </button>
                     </Link>
                 ))}
-            </div> */}
+            </div>
         </div>
     )
 }
